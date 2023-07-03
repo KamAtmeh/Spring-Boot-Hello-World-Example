add5a9eefc80cfca6cd51bd8020eae783419963c
KamAtmeh <75306898+KamAtmeh@users.noreply.github.com>
2023-07-03 11:38:27 +0200

    Update DemoApplicationTests.java

--- src/test/java/com/reytech/demo/DemoApplicationTests.java
+++ src/test/java/com/reytech/demo/DemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.reytech.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -10,4 +11,11 @@ class DemoApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+  	void groupedAssertions() {
+    		Personne pers = new Personne("Nassur", "Moumadi");
+
+    		assertAll("personne", () -> assertEquals("Nassur", pers.getFirstName()),
+        		() -> assertEquals("Moumadi", pers.getLastName()));
+	}
 }
